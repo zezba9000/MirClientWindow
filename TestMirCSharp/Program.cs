e9b9460a1923a0c4a49d393e0730bfbb4ea125a2
Andrew <zezba9000@gmail.com>
2024-03-04 15:55:26 -0800

    first commit

---
+++ TestMirCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TestMirCSharp
+{
+	unsafe class MainClass
+	{
+		const string lib = "libmirclient";
+
+		[DllImport(lib, EntryPoint = "mir_connect_sync", CharSet = CharSet.Ansi)]
+		static extern IntPtr mir_connect_sync(string server, string app_name);
+
+		public static void Main (string[] args)
+		{
+			var connection = mir_connect_sync ("TODO", "TODO");
+			Console.WriteLine ("Hello World: " + handle.ToString());
+			//mir_connection_release(connection);
+		}
+	}
+}
