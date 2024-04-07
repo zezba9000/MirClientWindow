9e9c6174a2c102751dd3f08d353fae44d62ddcae
Andrew <zezba9000@gmail.com>
2024-04-07 03:32:17 -0700

    fix build script

--- C/GLES2/build.sh
+++ C/GLES2/build.sh
@@ -1,1 +1,1 @@
-gcc main.c -I/usr/include/mirclient -I/usr/include/mircommon -I/usr/include/mircookie -I/usr/include/mircore -lmircore -lmirclient -lmircommon -lmircookie -lEGL -lGLESv2
+gcc main.c eglapp.c -I/usr/include/mirclient -I/usr/include/mircommon -I/usr/include/mircookie -I/usr/include/mircore -lmircore -lmirclient -lmircommon -lmircookie -lEGL -lGLESv2
